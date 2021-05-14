7762575ec32b82b035c6585f7de2cbfc81ca07f3
Arnav Jindal <arnav.jindal7@gmail.com>
2021-05-14 18:56:06 +0000

    Dotenv removal

--- src/main.rs
+++ src/main.rs
@@ -496,7 +496,7 @@ async fn main() {
     // This will load the environment variables located at `./.env`, relative to
     // the CWD. See `./.env.example` for an example on how to structure this.
 
-    dotenv::dotenv().expect("Failed to load .env file");
+    // dotenv::dotenv().expect("Failed to load .env file");
 
     // Initialize the logger to use environment variables.
     //
